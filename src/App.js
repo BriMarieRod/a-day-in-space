1cf757cfbc9048159a890e15a6596483f4547ff0
Brianna Marie <briannarodriguez@gmail.com>
2020-06-24 13:12:13 -0500

    Add responsive sizing to components

--- src/App.js
+++ src/App.js
@@ -5,23 +5,9 @@ class APODImage extends React.Component {
 
 	render() {
 		console.log(this.props);
-		console.log("APODImage render");
 		const data = this.props.data;
-		const originalWidth = this.props.width;
-		let width = this.props.isFullSize ? originalWidth : '672px';
-		// console.log(this.props.isFullSize);
-		//
-		/*
-		if(this.state.isLoaded) {
-			if(this.props.isFullSize) {
-				return <img className="APOD-img" src={data.url} alt={data.title} width={this.state.width} />;
-			} else {
-				return <img className="APOD-img" src={data.url} alt={data.title} width="50%" />;
-			}
-		} else {
-			return <img className="APOD-img" src={data.url} alt={data.title} width="50%" />;
-		}*/
-		return <a href={data.hdurl} className="APOD-img-link" style={{width: width,}}><img className="APOD-img" src={data.url} alt={data.title} width={width} /></a>;
+		let width = this.props.isFullSize ? this.props.size.width : this.props.windowSize.width*.35;
+		return <a href={data.hdurl} className="APOD-img-link" style={{width: width+'px',}}><img className="APOD-img" src={data.url} alt={data.title} width={width+'px'} /></a>;
 	}
 
 }
@@ -30,10 +16,14 @@ class APODContent extends React.Component {
 
 	constructor(props) {
 		super(props);
+		let halfSize = .70 * .5 * this.props.windowSize.width;
 		this.state = {
 			imageIsLoaded: false,
-			imageWidth: '672px',
-			imageHeight: null,
+			smallImageSize: halfSize,
+			imageSize: {
+				width: halfSize,
+				height: null,
+			},
 		};
 	}
 
@@ -43,28 +33,40 @@ class APODContent extends React.Component {
 			img.src = this.props.data.url;
 			let comp = this;
 			img.onload = function() {
-				console.log('width=' + this.width);
 				comp.setState({
 					imageIsLoaded: true,
-					imageWidth: this.width,
-					imageHeight: this.height,
+					imageSize: {
+						width: this.width,
+						height: this.height,
+					},
 				});
-				console.log(comp.state);
 			};
 		} else {
 			setTimeout( () => this.componentDidMount() , 300);
 		}
+		window.addEventListener("resize", () => this.updateDimensions() );
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("resize", () => this.updateDimensions() );
+	}
+
+	updateDimensions() {
+		this.setState({
+			imageSize: {
+				width: this.props.windowSize.width,
+				height: this.props.windowSize.height,
+			},
+		});
 	}
 
 	render() {
-		console.log("APODContent render");
-		//console.log(this.props);
-		if(this.props.data) {
-			const data = this.props.data;
-			//console.log(data);
-			// console.log(this.props.isImageFullSize);
+		//console.log(this.props.windowSize.width);
+		//console.log(this.state.imageSize);
+		let data = this.props.data;
+		if(data) {
 			return <div className="APOD-content">
-				<APODImage data={data} isFullSize={this.props.isImageFullSize} width={this.state.imageWidth} height={this.state.imageHeight} />
+				<APODImage data={data} isFullSize={this.props.isImageFullSize} size={this.state.imageSize} windowSize={this.props.windowSize} />
 				<div className="APOD-explanation">
 					<header>
 						<h3>{data.title}</h3>
@@ -93,7 +95,7 @@ class APOD extends React.Component {
 	componentDidMount() {
 		const url = 'https://api.nasa.gov/planetary/apod?date=2020-06-24&api_key=qbcsnvv4BEnmaiuI9RBgq5PGuDTs9Ih6eoaKsNu5';
 		fetch(url).then( res => res.json() ).then( json => {
-			console.log(json);
+			//console.log(json);
 			this.setState({
 				isFetched: true,
 				data: json,
@@ -108,14 +110,13 @@ class APOD extends React.Component {
 	}
 
 	render() {
-		console.log("APOD render");
 		return <article className="APOD">
 			<div className="container">
 				<header className="APOD-header">
 					<h2>Astronomy Picture of the Day</h2>
 					<button onClick={ () => this.handleImageSizeToggler() }>Toggle Full Size Image</button>
 				</header>
-				<APODContent isImageFullSize={this.state.isImageFullSize} data={this.state.data} value={this.state.isImageFullSize} />
+				<APODContent isImageFullSize={this.state.isImageFullSize} data={this.state.data} windowSize={this.props.windowSize} />
 			</div>
 		</article>;
 	}
@@ -124,19 +125,48 @@ class APOD extends React.Component {
 class AppContent extends React.Component {
 	render() {
 		return <main>
-			<APOD />
+			<APOD windowSize={this.props.windowSize} />
 		</main>;
 	}
 }
 
 class App extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			windowSize: {
+				width: window.innerWidth,
+				height: window.innerHeight,
+			},
+		};
+	}
+
+	componentDidMount() {
+		window.addEventListener("resize", () => this.updateDimensions() );
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("resize", () => this.updateDimensions() );
+	}
+
+	updateDimensions() {
+		console.log("Updating");
+		this.setState({
+			windowSize: {
+				width: window.innerWidth,
+				height: window.innerHeight,
+			},
+		});
+	}
+
 	render() {
+		//console.log(this.state);
 		return <div className="App">
 			<header className="App-header">
 				<h1>A Day In Space</h1>
 			</header>
-			<AppContent />
+			<AppContent windowSize={this.state.windowSize} />
 		</div>;
 	}
 
